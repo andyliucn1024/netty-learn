947c479f5c8581fa52f310efa27298db17d40404
liulei <liulei_work@163.com>
2019-06-16 20:06:53 +0800

    群聊 服务器端已完成

--- src/main/java/com/liulei/nettylearn/example3/MyChatServerHandler.java
+++ src/main/java/com/liulei/nettylearn/example3/MyChatServerHandler.java
@@ -1,4 +1,68 @@
 package com.liulei.nettylearn.example3;
 
-public class MyChatServerHandler {
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    /**
+     * <strong>Please keep in mind that this method will be renamed to
+     * {@code messageReceived(ChannelHandlerContext, I)} in 5.0.</strong>
+     * <p>
+     * Is called for each message of type {@link I}.
+     *
+     * @param ctx the {@link ChannelHandlerContext} which this {@link SimpleChannelInboundHandler}
+     *            belongs to
+     * @param msg the message to handle
+     * @throws Exception is thrown if an error occurred
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.forEach(channel1 -> {
+            if(channel!= channel)
+                channel1.writeAndFlush(channel1.remoteAddress()+"发来消息: "+msg+"\n");
+            else
+                    channel1.writeAndFlush("您发送了一条消息: "+msg+"\n");
+        });
+
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel  = ctx.channel();
+        channelGroup.writeAndFlush("客户端 - "+channel.remoteAddress()+"加入\n");
+        channelGroup.add(channel);
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.remove(channel);
+        channelGroup.writeAndFlush("客户端 - "+channel.remoteAddress()+"移除\n");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress()+" 上线啦！！！");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx
+                .channel();
+        System.out.println(channel.remoteAddress()+" 下线");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
