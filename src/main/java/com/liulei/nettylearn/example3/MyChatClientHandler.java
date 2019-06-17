e2a670459635133b5714163a9337f8a1e9cd65ae
liulei.bj <liulei.bj@fang.com>
2019-06-17 16:41:33 +0800

    服务器上线下线测试成功 客户端收消息暂未成功

---
+++ src/main/java/com/liulei/nettylearn/example3/MyChatClientHandler.java
@@ -0,0 +1,40 @@
+package com.liulei.nettylearn.example3;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.time.LocalDateTime;
+
+public class MyChatClientHandler extends SimpleChannelInboundHandler<String> {
+    /**
+     * Calls {@link ChannelHandlerContext#fireChannelActive()} to forward
+     * to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+     * <p>
+     * Sub-classes may override this method to change behavior.
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("来自客户端的问候");
+    }
+
+    /**
+     * <strong>Please keep in mind that this method will be renamed to
+     * {@code messageReceived(ChannelHandlerContext, I)} in 5.0.</strong>
+     * <p>
+     * Is called for each message of type {@link I}.
+     *
+     * @param ctx the {@link ChannelHandlerContext} which this {@link SimpleChannelInboundHandler}
+     *            belongs to
+     * @param msg the message to handle
+     * @throws Exception is thrown if an error occurred
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress()+msg);
+
+        ctx.writeAndFlush("time:::"+ LocalDateTime.now());
+//        ctx.close();
+    }
+}
